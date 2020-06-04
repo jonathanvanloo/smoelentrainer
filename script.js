94ff52c514d62b7f40183233d2680f21bfb0658c
jonathan <99052897@mydavinci.nl>
2020-06-04 12:47:02 +0200

    #2
    3-6

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var spelers = [["img/2020-06-03 14_41_21-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Nicolai Jorgensen"], 
+["img/2020-06-03 14_43_03-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Robin van Persie"], 
+["img/2020-06-03 14_43_29-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Steven Berghuis"], 
+["img/2020-06-03 14_44_51-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Luis Sinisterra"], 
+["img/2020-06-03 14_45_24-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Sam Larsson"], 
+["img/2020-06-03 14_46_14-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Cheick Toure"], 
+["img/2020-06-03 14_46_37-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Jens Toornstra"], 
+["img/2020-06-03 14_47_03-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Tonny Vilhena"], 
+["img/2020-06-03 14_47_19-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Yassin Ayoub"], 
+["img/2020-06-03 14_47_39-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Renato Tapia"], 
+["img/2020-06-03 14_47_57-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Ridgeciano Haps"], 
+["img/2020-06-03 14_48_23-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Jeremiah St.Juste"],
+["img/2020-06-03 14_48_41-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Sven van Beek"], 
+["img/2020-06-03 14_49_04-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Jan Arie van der Heijden"], 
+["img/2020-06-03 14_49_49-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Eric Botteghin"], 
+["img/2020-06-03 14_50_06-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Tyrell Malacia"], 
+["img/2020-06-03 14_50_26-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Bart Nieuwkoop"], 
+["img/2020-06-03 14_51_20-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Justin Bijlow "], 
+["img/2020-06-03 14_51_47-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Kenneth Vermeer"], 
+["img/2020-06-03 14_52_15-Selectie 2018 – 2019 – Feyenoord in Beeld.png", "Ramon ten Hove"]];
+
+
+document.getElementById("img1").src = spelers[0][0];
+document.getElementById("name1").innerHTML = spelers[0][1];
+document.getElementById("img2").src = spelers[1][0];
+document.getElementById("name2").innerHTML = spelers[1][1];
+document.getElementById("img3").src = spelers[2][0];
+document.getElementById("name3").innerHTML = spelers[2][1];
+document.getElementById("img4").src = spelers[3][0];
+document.getElementById("name4").innerHTML = spelers[3][1];
+document.getElementById("img5").src = spelers[4][0];
+document.getElementById("name5").innerHTML = spelers[4][1];
+
+// document.write('<table>')
+
+// for (var i = 0; i < spelers.length; i++) {
+// 	document.write('<tr><td>' + spelers[i] + '</tr></td>')
+// }
+
+// document.write('</table>')
