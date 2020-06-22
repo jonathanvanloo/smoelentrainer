33f98554a093cf527aec7b82c3781e068334b3ba
jonathan <99052897@mydavinci.nl>
2020-06-22 10:14:17 +0200

    nog van vrijdag 20-6

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 var spelers = [
 {image: "img/2020-06-03 14_41_21-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Nicolai Jorgensen"},
 {image: "img/2020-06-03 14_43_03-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Robin van Persie"},
-{image: "img/2020-06-03 14_43_03-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Robin van Persie"},
 {image: "img/2020-06-03 14_43_29-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Steven Berghuis"},
 {image: "img/2020-06-03 14_44_51-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Luis Sinisterra"},
 {image: "img/2020-06-03 14_45_24-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Sam Larsson"},
@@ -23,24 +22,20 @@ var spelers = [
 {image: "img/2020-06-03 14_52_15-Selectie 2018 – 2019 – Feyenoord in Beeld.png", name: "Ramon ten Hove"},
 ];
 // make kopie van spelers in images (slice)
-
+var images = spelers.slice();
 // make kopie van spelers in names (slice)
-
+var names = spelers.slice();
 //  zolang images nog een element bevat: vul ee element met een image, verwijder daarna ook de image uit images
-var a = 1 ;
-while  (a < 6){
-	const randomElement = spelers[Math.floor(Math.random() * spelers.length)];
-	var een = document.createElement("img");
-	var twee = document.createElement("h2");
-	var drie = document.createTextNode(randomElement.name);
-	een.setAttribute("src", randomElement.image);
-	document.body.appendChild(een);
-	console.log(een)
-	console.log(twee)
-	console.log(drie)
-	twee.appendChild(drie)
-	document.body.appendChild(drie);
-	a++;
+while  (spelers.length > 0){
+	var index = Math.floor(Math.random() * spelers.length);
+	const randomElement = spelers[index];
+	var img = document.createElement("img");
+	var h2 = document.createElement("h2");
+	h2.innerHTML = randomElement.name
+	img.setAttribute("src", randomElement.image);
+	document.body.appendChild(img);
+	document.body.appendChild(h2);
+	dell = spelers.splice(index, 1);
 }
 
 // function click(id) {
